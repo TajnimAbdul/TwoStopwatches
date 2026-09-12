430be3ee282400c25850a78401557d52e3d6863e
Tajnim Abdul <tajnimabdul2007@gmail.com>
2026-09-12 10:55:43 +0100

    Initial Commit

---
+++ TwoStopwatches/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TwoStopwatches
+//
+//  Created by Tajnim Abdul on 12/09/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TwoStopwatches/TwoStopwatchesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwoStopwatchesApp.swift
+//  TwoStopwatches
+//
+//  Created by Tajnim Abdul on 12/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct TwoStopwatchesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
